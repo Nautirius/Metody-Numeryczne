0483d89c0415cdb7bb2d85a3888fdda1ecea1d0d
Nautirius <marcinknapczyk02@gmail.com>
2024-03-10 00:43:57 +0100

    initial commit

---
+++ lab01/solution/matrix_invertion.c
@@ -0,0 +1,176 @@
+// #include "/usr/include/gsl/include/gsl/gsl_math.h"
+// #include "/usr/include/gsl/include/gsl/gsl_linalg.h"
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_linalg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int print_matrix(FILE *f, const gsl_matrix *m)
+{
+    int status, n = 0;
+
+    for (size_t i = 0; i < m->size1; i++)
+    {
+        for (size_t j = 0; j < m->size2; j++)
+        {
+            printf("%.6g\t", gsl_matrix_get(m, i, j));
+            if ((status = fprintf(f, "%.6g\t", gsl_matrix_get(m, i, j))) < 0)
+                return -1;
+            n += status;
+        }
+        printf("\n");
+
+        if ((status = fprintf(f, "\n")) < 0)
+            return -1;
+        n += status;
+    }
+
+    return n;
+}
+void find_diagonal(const gsl_matrix *m, double *diag)
+{
+    for (size_t i = 0; i < m->size1; i++)
+        diag[i] = gsl_matrix_get(m, i, i);
+}
+
+int main()
+{
+    // inicjalizacja
+    int m = 4;
+    int n = 4;
+    gsl_matrix *A = gsl_matrix_calloc(n, m);
+    gsl_matrix *A_COPY = gsl_matrix_calloc(n, m);
+
+    int RO = 2;
+
+    // wypelnianie macierzy wartosciami
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            double value = 1. / (double)(i + j + RO);
+            gsl_matrix_set(A, i, j, value);
+            gsl_matrix_set(A_COPY, i, j, value);
+        }
+    }
+
+    //
+    // szukamy rozkladu LU
+    gsl_permutation *p = gsl_permutation_calloc(m);
+
+    // int gsl_linalg_LU_decomp(gsl_matrix *a, gsl permutation *p, int *signum);
+    int sig = 1;
+    gsl_linalg_LU_decomp(A, p, &sig);
+
+    //
+    // macierz dolna - na diagonali jedynki
+    // szukanie diagonali
+    double *diag = calloc(A->size1, sizeof(double));
+    find_diagonal(A, diag);
+
+    FILE *fp = fopen("wyniki.txt", "w");
+
+    double det = 1;
+    fprintf(fp, "Diagonala macierzy U: ");
+    for (int i = 0; i < m; i++)
+    {
+        fprintf(fp, "%.6f\t", diag[i]);
+        det *= diag[i];
+    }
+    fprintf(fp, "\n\nWyznacznik macierzy A: %g\n", det);
+
+    //
+    // wyznaczanie macierzy odwrotnej
+
+    // int gsl_linalg_LU_solve(gsl_matrix *A, gsl_permutation *p, gsl_vector *b, gsl_vector *x);
+
+    gsl_vector *b1 = gsl_vector_calloc(m);
+    gsl_vector_set(b1, 0, 1.);
+    gsl_vector *b2 = gsl_vector_calloc(m);
+    gsl_vector_set(b2, 1, 1.);
+    gsl_vector *b3 = gsl_vector_calloc(m);
+    gsl_vector_set(b3, 2, 1.);
+    gsl_vector *b4 = gsl_vector_calloc(m);
+    gsl_vector_set(b4, 3, 1.);
+
+    gsl_vector *x1 = gsl_vector_calloc(m);
+    gsl_vector *x2 = gsl_vector_calloc(m);
+    gsl_vector *x3 = gsl_vector_calloc(m);
+    gsl_vector *x4 = gsl_vector_calloc(m);
+
+    gsl_linalg_LU_solve(A, p, b1, x1);
+    gsl_linalg_LU_solve(A, p, b2, x2);
+    gsl_linalg_LU_solve(A, p, b3, x3);
+    gsl_linalg_LU_solve(A, p, b4, x4);
+
+    gsl_matrix *SOLUTION = gsl_matrix_calloc(n, m);
+
+    for (int i = 0; i < n; i++)
+    {
+        gsl_matrix_set(SOLUTION, i, 0, gsl_vector_get(x1, i));
+        gsl_matrix_set(SOLUTION, i, 1, gsl_vector_get(x2, i));
+        gsl_matrix_set(SOLUTION, i, 2, gsl_vector_get(x3, i));
+        gsl_matrix_set(SOLUTION, i, 3, gsl_vector_get(x4, i));
+    }
+
+    fprintf(fp, "\nMacierz odwrotna:\n");
+    print_matrix(fp, SOLUTION);
+
+    //
+    // liczenie iloczynu macierzy
+
+    gsl_matrix *PRODUCT = gsl_matrix_calloc(n, m);
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            double sum = 0.0;
+            for (int k = 0; k < n; k++)
+            {
+                sum += gsl_matrix_get(A_COPY, i, k) * gsl_matrix_get(SOLUTION, k, j);
+            }
+            gsl_matrix_set(PRODUCT, i, j, sum);
+        }
+    }
+    fprintf(fp, "\nWynik iloczynu:\n");
+    print_matrix(fp, PRODUCT);
+    // printf("\n");
+    // print_matrix(fp, A_COPY);
+
+    //
+    // wyznaczanie wskaźnika uwarunkowania macierzy
+
+    double norm1 = 0.0, norm2 = 0.0;
+    double h1, h2;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            h1 = fabs(gsl_matrix_get(A_COPY, i, j));
+            h2 = fabs(gsl_matrix_get(SOLUTION, j, i));
+            if (h1 > norm1)
+                norm1 = h1;
+            if (h2 > norm2)
+                norm2 = h2;
+        }
+    }
+    double cond = norm1 * norm2;
+    fprintf(fp, "\nWskaźnik uwarunkowania:\n%f", cond);
+
+    //
+    // zwalnianie pamięci
+    gsl_matrix_free(PRODUCT);
+    gsl_matrix_free(SOLUTION);
+    gsl_vector_free(b1);
+    gsl_vector_free(x1);
+
+    fclose(fp);
+    free(diag);
+    gsl_permutation_free(p);
+    gsl_matrix_free(A_COPY);
+    gsl_matrix_free(A);
+
+    return 0;
+}
---
+++ lab02/solution/gauss_elimination.c
@@ -0,0 +1,184 @@
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_linalg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_matrix(const gsl_matrix *M)
+{
+    printf("\nMacierz:\n");
+    for (size_t row = 0; row < M->size1; ++row)
+    {
+        for (size_t col = 0; col < M->size2; ++col)
+        {
+            printf("\t%3.1f", gsl_matrix_get(M, row, col));
+        }
+        printf("\n");
+    }
+}
+void print_vector(const gsl_vector *V)
+{
+    printf("\nWektor:\n");
+    for (size_t col = 0; col < V->size; ++col)
+    {
+        printf("\t%3.1f", gsl_vector_get(V, col));
+    }
+    printf("\n");
+}
+void fill_matrix(gsl_matrix *M, double *values)
+{
+    for (size_t row = 0; row < M->size1; ++row)
+    {
+        for (size_t col = 0; col < M->size2; ++col)
+        {
+            gsl_matrix_set(M, row, col, values[M->size2 * row + col]);
+        }
+    }
+}
+void fill_vector(gsl_vector *V, double *values)
+{
+    for (size_t col = 0; col < V->size; ++col)
+    {
+        gsl_vector_set(V, col, values[col]);
+    }
+}
+void gauss_elim_solve(gsl_matrix *A, gsl_vector *V)
+{
+    //
+    // rozwiazywanie układu
+    //
+    // zerowanie poddiagonali
+    // iterowanie po kolumnach
+    for (int i = 0; i < A->size2 - 1; i++)
+    {
+        // dla kazdej kolumny pobieram wartosc a_nn
+        double a = gsl_matrix_get(A, i, i);
+
+        // dzielimy wiersz przez a_nn
+        for (int j = 0; j < A->size2; j++)
+        {
+            gsl_matrix_set(A, i, j, gsl_matrix_get(A, i, j) / a);
+        }
+        gsl_vector_set(V, i, gsl_vector_get(V, i) / a);
+
+        // zerowanie poddiagonali
+        // iterowanie po wierszach
+        for (int j = i + 1; j < A->size1; j++)
+        {
+            double mnoznik = gsl_matrix_get(A, j, i);
+
+            // iterowanie po kolumnach
+            for (int k = 0; k < A->size2; k++)
+            {
+                double gora = gsl_matrix_get(A, i, k);
+                gsl_matrix_set(A, j, k, gsl_matrix_get(A, j, k) - gora * mnoznik);
+            }
+            gsl_vector_set(V, j, gsl_vector_get(V, j) - mnoznik * gsl_vector_get(V, i));
+        }
+    }
+    // jedynkownaie ostatniego wiersza
+    gsl_vector_set(V, V->size - 1, gsl_vector_get(V, V->size - 1) / gsl_matrix_get(A, A->size1 - 1, A->size2 - 1));
+    gsl_matrix_set(A, A->size1 - 1, A->size2 - 1, 1);
+
+    printf("\nPo zerowaniu dołu:");
+    print_matrix(A);
+    print_vector(V);
+
+    //
+    // zerowanie naddiagonali
+    // iterowanie po kolumnach
+    for (int j = A->size2 - 1; j >= 0; j--)
+    {
+        double mnoznik = gsl_vector_get(V, j);
+
+        // iterowanie po wierszach
+        for (int i = j - 1; i >= 0; i--)
+        {
+            double wyraz = gsl_matrix_get(A, i, j);
+            gsl_vector_set(V, i, gsl_vector_get(V, i) - wyraz * mnoznik);
+            gsl_matrix_set(A, i, j, mnoznik - mnoznik);
+        }
+    }
+
+    printf("\nPo zerowaniu góry:");
+    print_matrix(A);
+    print_vector(V);
+}
+void matrix_vector_multiplication(gsl_matrix *M, gsl_vector *V, gsl_vector *C)
+{
+    for (size_t row = 0; row < M->size1; ++row)
+    {
+        double sum = 0.0;
+        for (size_t col = 0; col < M->size2; ++col)
+        {
+            sum += gsl_matrix_get(M, row, col) * gsl_vector_get(V, col);
+        }
+        gsl_vector_set(C, row, sum);
+    }
+}
+double calculate_deviation(gsl_vector *V1, gsl_vector *V2)
+{
+    double sum = 0.0;
+    for (size_t row = 0; row < V1->size; ++row)
+    {
+        double diff = gsl_vector_get(V2, row) - gsl_vector_get(V1, row);
+        sum += diff * diff;
+    }
+    return sqrt(sum) / 5.0;
+}
+
+int main()
+{
+    FILE *fp = fopen("wyniki.txt", "w");
+    int m = 5, n = 5;
+    double a_data[] = {
+        0, 1, 6, 9, 10,
+        2, 1, 6, 9, 10,
+        1, 6, 6, 8, 6,
+        5, 9, 10, 7, 10,
+        3, 4, 9, 7, 9};
+    double b_data[] = {10, 2, 9, 9, 3};
+
+    gsl_matrix *A = gsl_matrix_calloc(m, n);
+    gsl_matrix *A_CP = gsl_matrix_calloc(m, n);
+    gsl_vector *b = gsl_vector_calloc(n);
+    gsl_vector *b_cp = gsl_vector_calloc(n);
+    gsl_vector *c = gsl_vector_calloc(n);
+
+    for (double q = 0.01; q < 3.0; q += 0.01)
+    {
+        // zmiana wartości w macierzy A i wektorze b
+        fill_matrix(A, a_data);
+        fill_matrix(A_CP, a_data);
+        fill_vector(b, b_data);
+        fill_vector(b_cp, b_data);
+        gsl_matrix_set(A, 0, 0, 2.0 * q);
+        gsl_matrix_set(A_CP, 0, 0, 2.0 * q);
+
+        printf("\n\nMacierz dla q=%f:\n", q);
+        print_matrix(A);
+        print_vector(b);
+
+        // rozwiązanie układu
+        gauss_elim_solve(A, b);
+
+        // mnożenie A*b
+        matrix_vector_multiplication(A_CP, b, c);
+        printf("\nWektor C:");
+        print_vector(c);
+
+        // obliczanie odchylenia między wektorami b i c
+        double o = calculate_deviation(b_cp, c);
+        printf("\nOdchylenie między wektorami b i c: %f\n", o);
+
+        fprintf(fp, "%.2f,%f\n", q, o);
+    }
+
+    fclose(fp);
+    gsl_matrix_free(A);
+    gsl_matrix_free(A_CP);
+    gsl_vector_free(b);
+    gsl_vector_free(b_cp);
+    gsl_vector_free(c);
+
+    return 0;
+}
